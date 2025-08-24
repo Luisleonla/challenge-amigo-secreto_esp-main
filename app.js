6e991b8e69a8b62b538f2021ee16c9033c6e19dd
Luis Leon <luis.ant.arti.l@gmail.com>
2025-08-23 23:28:13 -0600

    Factorización de código

--- app.js
+++ app.js
@@ -3,60 +3,45 @@ let arrayAmigos = [];
 let listaAmigos = document.getElementById('listaAmigos');
 let listaResultado = document.getElementById('resultado');
 
+estadoInicial();
+
+//Creación de la función que agregará cada amigo nuevo al arrayAmigos y a la lista que se mostrará en nuestro documento HTML.
 function agregarAmigo() {
-    // Se usa document.getElementById().value para extraer el valor de la caja Input usando el Id asignado en el documento HTML.
-    // let inputAmigo = document.getElementById('amigo');
-    // let valorAmigo = inputAmigo.value;
-
+    //Se extrae el nombre ingresado por el usuario
     let nombre = document.querySelector('#amigo').value;
-    console.log(nombre);
-
-    // Verifica si el nombre está vacío
-    if(nombre == ""){
-        alert ("Por favor, inserte un nombre.");
-        return;
-    }
-
-    // Expresión regular: solo letras y espacios
+    
+    // Verifica que la entrada no sea una valor vacío.
+    // Excluye valores que tengan numeros o caracteres especiales.
     if(!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre)){
-        alert("El nombre solo debe contener letras y espacios.");
-        limpiarCaja();
+        alert("Verifique que la entrada sea correcta.\n•NO agregar un valor vacío.\n•Solo se permiten letras, espacios y acentos.");
+        estadoInicial();
         return;
     }
 
-    //En caso de que la entrada de texto sea validada como correcta, se procede a guardarla en el array listaAmigos.
+    //Cuando la entrada sea validada como correcta, se guarda en la variable arrayAmigos.
     arrayAmigos.push(nombre);
-    limpiarCaja();
-    console.log(arrayAmigos);
-
-    //limpiarListaHTML(listaAmigos);
-    limpiarListaHTML(listaResultado); // Limpia la lista antes de volver a llenarla
-
-    // let lista = document.getElementById('listaAmigos');
+    estadoInicial();
     
-    //Crea un nuevo elemento <li>
-    let nuevoElemento = document.createElement('li');
-    nuevoElemento.textContent = nombre;
-    
-    //Agrega el <li> a la <ul>
-    listaAmigos.appendChild(nuevoElemento);
+    // Limpia las listas "listaAmigos" y "listaResultados" para evitar resultados duplicados.
+    limpiarListaHTML(listaAmigos);
+    limpiarListaHTML(listaResultado); 
 
-   
-    // for (let i = 0; i < arrayAmigos.length; i++) {        
-    //     crearElementoLista(arrayAmigos[i], listaAmigos);
-    // }
-
-    
-    
+    //Iteramos la lectura de los elementos del arrayAmigos para agragarlos uno por uno a la lista HTML de amigos.
+    for (let i = 0; i < arrayAmigos.length; i++) {        
+        crearElementoLista(arrayAmigos[i], listaAmigos);
+    }   
 }
 
+//Función que escogerá un amigo aleatoriamente de entre la lista de amigos dada. Mostrará en pantalla el amigo elegido y reiniciará las variables.
 function sortearAmigo() {
+    //Condición que descartará el comando de sortear amigo cuando la lista no cumpla con al menos 2 elementos.
     if(arrayAmigos.length < 2) {
         alert("Debe ingresar al menos 2 amigos.")
+        estadoInicial();
     }else{
-        
+        //Cuando existan el mínimo de elementos necesarios para ser sorteados, se realizará la selección aleatoria y será mostrada en pantalla 
+        estadoInicial();
         let indexAmigo = Math.floor(Math.random() * arrayAmigos.length);
-        limpiarListaHTML(listaResultado);
         crearElementoLista(arrayAmigos[indexAmigo], listaResultado);
         limpiarListaHTML(listaAmigos);
         arrayAmigos = [];
@@ -64,17 +49,17 @@ function sortearAmigo() {
     }
 }
 
-
-
-function limpiarCaja() {
+function estadoInicial() {
     document.querySelector('#amigo').value = '';
+    //Adicionalmente agregamos un comando para posiciar el cursor nuevamente en la caja input para que la entrada de datos sea mas fácil para el usuario.
+    document.querySelector('#amigo').focus();
 }
 
 function limpiarListaHTML(listaHTML) {
     listaHTML.innerHTML = '';
 }
 
-function crearElementoLista(nombre, listaHTML) {6    
+function crearElementoLista(nombre, listaHTML) {    
     //Crea un nuevo elemento <li>
     let nuevoElemento = document.createElement('li');
     nuevoElemento.textContent = nombre;
