0223e9bc9c09d583e8397b5ea4cf396aa5182b08
Luis Leon <luis.ant.arti.l@gmail.com>
2025-08-23 20:46:44 -0600

    Código terminado sin el uso de for

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // El principal objuetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let arrayAmigos = [];
+let listaAmigos = document.getElementById('listaAmigos');
+let listaResultado = document.getElementById('resultado');
 
 function agregarAmigo() {
     // Se usa document.getElementById().value para extraer el valor de la caja Input usando el Id asignado en el documento HTML.
@@ -23,23 +25,59 @@ function agregarAmigo() {
     }
 
     //En caso de que la entrada de texto sea validada como correcta, se procede a guardarla en el array listaAmigos.
-    listaAmigos.push(nombre);
+    arrayAmigos.push(nombre);
     limpiarCaja();
-    console.log(listaAmigos);
+    console.log(arrayAmigos);
 
-    let lista = document.getElementById('listaAmigos');
+    //limpiarListaHTML(listaAmigos);
+    limpiarListaHTML(listaResultado); // Limpia la lista antes de volver a llenarla
+
+    // let lista = document.getElementById('listaAmigos');
     
     //Crea un nuevo elemento <li>
     let nuevoElemento = document.createElement('li');
     nuevoElemento.textContent = nombre;
     
     //Agrega el <li> a la <ul>
-    lista.appendChild(nuevoElemento);
+    listaAmigos.appendChild(nuevoElemento);
+
+   
+    // for (let i = 0; i < arrayAmigos.length; i++) {        
+    //     crearElementoLista(arrayAmigos[i], listaAmigos);
+    // }
+
+    
     
 }
 
-//console.log(agregarAmigo.toString());
+function sortearAmigo() {
+    if(arrayAmigos.length < 2) {
+        alert("Debe ingresar al menos 2 amigos.")
+    }else{
+        
+        let indexAmigo = Math.floor(Math.random() * arrayAmigos.length);
+        limpiarListaHTML(listaResultado);
+        crearElementoLista(arrayAmigos[indexAmigo], listaResultado);
+        limpiarListaHTML(listaAmigos);
+        arrayAmigos = [];
+        console.log(indexAmigo);
+    }
+}
 
-function limpiarCaja () {
+
+
+function limpiarCaja() {
     document.querySelector('#amigo').value = '';
+}
+
+function limpiarListaHTML(listaHTML) {
+    listaHTML.innerHTML = '';
+}
+
+function crearElementoLista(nombre, listaHTML) {6    
+    //Crea un nuevo elemento <li>
+    let nuevoElemento = document.createElement('li');
+    nuevoElemento.textContent = nombre;
+    //Agrega el <li> a la <ul>
+    listaHTML.appendChild(nuevoElemento);
 }
